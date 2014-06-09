96f310ead2ae1139aadb998217a0178bea14d7d0
Christian Eder <ederc@mathematik.uni-kl.de>
2014-06-09 18:15:43 +0200

    1. removes m4ri debug option in config 2. adds first version of jcf matrix read

--- cli/io.c
+++ cli/io.c
@@ -4,16 +4,11 @@
 sparse_mat_t *load_matrix_jcf_format(const char *fn, int verbose) {
 
   // start loading the matrix
-  uint16 *nz;
-  uint32 *pos;
-  uint32 sz;
-  uint32 n;
-  uint32 m;
-  uint32 mod;
-  uint32 nb;
-  uint64 fl;
-
-  sparse_mat_t *A = NULL;
+  row_dim_t m;
+  col_dim_t n;
+  mod_t     mod;
+  nnz_t     nnz;
+  uint64    fl;
 
   // open in binary mode first to get file size with fseek
   FILE *fh        = fopen(fn,"rb");
@@ -29,34 +24,38 @@ sparse_mat_t *load_matrix_jcf_format(const char *fn, int verbose) {
   // now read data from file
   fh  = fopen(fn,"r");
   // get columns
-  if (fread(&n, sizeof(uint32), 1, fh) != 1) {
-    if (verbose)
-      printf("Error while reading file '%s'\n",fn);
-    return NULL;
-  }
-  // get rows
   if (fread(&m, sizeof(uint32), 1, fh) != 1) {
     if (verbose)
       printf("Error while reading file '%s'\n",fn);
+    fclose(fh);
+    return NULL;
+  }
+  // get rows
+  if (fread(&n, sizeof(uint32), 1, fh) != 1) {
+    if (verbose)
+      printf("Error while reading file '%s'\n",fn);
+    fclose(fh);
     return NULL;
   }
   // get modulus
   if ((fread(&mod, sizeof(uint32), 1, fh) != 1) || (mod == 1)) {
     if (verbose)
       printf("Error while reading file '%s'\n",fn);
+    fclose(fh);
     return NULL;
   }
   // get number of nonzero elements
-  if (fread(&nb, sizeof(uint64), 1, fh) != 1) {
+  if (fread(&nnz, sizeof(uint64), 1, fh) != 1) {
     if (verbose)
       printf("Error while reading file '%s'\n",fn);
+    fclose(fh);
     return NULL;
   }
 
   if (verbose) {
     // density of matrix
     double density  = (double) n * (double) m;
-    density   =   (double) (nb) / density;
+    density   =   (double) (nnz) / density;
     density   *=  100.0;
     // file size of matrix
     double fs =   (double) fl / 1024 / 1024;
@@ -69,13 +68,84 @@ sparse_mat_t *load_matrix_jcf_format(const char *fn, int verbose) {
     printf("Data for %s\n", fn);
     printf("--------------------------------------------------------------\n");
     printf("modulus                     %14d\n", mod);
-    printf("number of rows              %14d\n", n);
-    printf("number of columns           %14d\n", m);
-    printf("number of nonzero elements  %14ld\n", nb);
+    printf("number of rows              %14d\n", m);
+    printf("number of columns           %14d\n", n);
+    printf("number of nonzero elements  %14ld\n", nnz);
     printf("density                     %14.2f%\n", density);
     printf("size                        %14.2f%s\n", fs, fsu);
     printf("--------------------------------------------------------------\n");
   }
+
+  // read entries from file
+  sparse_mat_t *A = (sparse_mat_t *)malloc(sizeof(sparse_mat_t));
+  A->rows         = (entry_t **)malloc(sizeof(entry_t **));
+  A->pos          = (col_idx_t **)malloc(sizeof(col_idx_t **));
+  // get meta data
+  A->nrows   = m;
+  A->ncols   = n;
+  A->nnz     = nnz;
+  A->mod     = mod;
+  
+  // maximal possible nonzero entries per row is n*sizeof(entry_t)
+  entry_t   *nze  = (entry_t *)malloc(n * sizeof(entry_t));
+  col_idx_t *pos  = (col_idx_t *)malloc(n * sizeof(col_idx_t));
+
+  // store header size of file
+  // size of m, n, mod and nb
+  uint32 hs  = 3 * sizeof(uint32) + sizeof(uint64);
+
+  // offsets for file handling
+  uint64 row_size_offset = nnz * sizeof(entry_t) + nnz * sizeof(uint32) + hs;
+  uint64 row_val_offset = hs;
+  uint64 row_pos_offset = nnz * sizeof(entry_t) + hs;
+
+  uint32 sz;
+  uint32 i, j;
+
+  for (i = 0; i < m; ++i) {
+    fseek(fh, row_size_offset, SEEK_SET);
+    if (fread(&sz, sizeof(col_idx_t), 1, fh) != 1) {
+      if (verbose)
+        printf("Error while reading file '%s'\n",fn);
+      free(A);
+      fclose(fh);
+      return NULL;
+    }
+
+    row_size_offset +=  sizeof(uint32);
+
+    fseek(fh, row_val_offset, SEEK_SET);
+    if (fread(nze, sizeof(entry_t), sz, fh) != sz) {
+      if (verbose)
+        printf("Error while reading file '%s'\n",fn);
+      free(A);
+      fclose(fh);
+      return NULL;
+    }
+
+    row_val_offset +=  sz * sizeof(entry_t);
+
+    fseek(fh, row_pos_offset, SEEK_SET);
+    if (fread(pos, sizeof(col_idx_t), sz, fh) != sz) {
+      if (verbose)
+        printf("Error while reading file '%s'\n",fn);
+      free(A);
+      fclose(fh);
+      return NULL;
+    }
+
+    row_pos_offset +=  sz * sizeof(uint32);
+
+    // reserve memory in matrix A for row[i]
+    A->rows[i] = (entry_t *)malloc(sz * sizeof(entry_t));
+    A->pos[i]  = (col_idx_t *)malloc(sz * sizeof(col_idx_t));
+    for (j = 0; j < sz; ++j) {
+      A->rows[i][j]  = nze[j];
+      A->pos[i][j]   = pos[j];
+    }
+  }
+
+  fclose(fh); 
   return A;
 }
 
--- src/sparse_matrix.h
+++ src/sparse_matrix.h
@@ -14,18 +14,25 @@
 typedef uint16  entry_t;
 typedef uint32  col_idx_t;
 typedef uint32  row_idx_t;
+typedef uint32  col_dim_t;
+typedef uint32  row_dim_t;
+typedef uint64  nnz_t;
+typedef uint32  mod_t;
 
-// rows
-typedef struct row_t {
-  entry_t   *first_entry;
-  entry_t   *last_entry;
-  col_idx_t *first_col;
-  col_idx_t *last_col;
-} row_t;
+/**
+ * \brief Sparse matrix structure for reading jcf matrices
+ *
+ */
 
-// sparse matrices
 typedef struct sparse_mat_t {
-  row_t **rows;
+  row_dim_t nrows;    /*!< number of rows */
+  col_dim_t ncols;    /*!< number of columns */
+  nnz_t     nnz;      /*!< number of nonzero entries */
+  mod_t     mod;      /*!< modulo/field characteristic */
+  entry_t   **rows;   /*!< address of row: m->row[i] gives first
+                           address of ith row */
+  col_idx_t **pos;    /*!< position of entry in row: m->pos[i] gives first
+                           address of first position of nonzero entry in row i */
 } sparse_mat_t;
 
 #endif
