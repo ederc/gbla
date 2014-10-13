618c71e5631a35ffc9276380cc1dc5be29ebc981
Christian Eder <ederc@mathematik.uni-kl.de>
2014-10-13 15:26:36 +0200

    adds interface for blocked A^-1B computation for conversion of Martani's LELA code

--- src/elimination.c
+++ src/elimination.c
@@ -1,9 +1,78 @@
 #include "elimination.h"
 
-int elim_fl_A_block(sbm_fl_t *A, sbm_fl_t *B) {
+int elim_fl_A_block(sbm_fl_t *A, sbm_fl_t *B, int nthrds) {
+  ci_t i, rc;
+  const ci_t clB  = (ci_t) ceil((float) B->ncols / B->bwidth);
+  const ri_t rlA  = (ci_t) ceil((float) A->nrows / A->bheight);
+  #pragma omp parallel num_threads(nthrds)
+  {
+    #pragma omp for
+      // each task takes one block column of B
+      for (i=0; i<clB; ++i) {
+        #pragma omp task
+        {
+          rc  = elim_fl_A_blocks_task(A, B, i, rlA);
+        }
+      }
+    #pragma omp taskwait
+  }
   return 0;
 }
 
-int elim_fl_A_ml_block(sm_fl_ml_t *A, sbm_fl_t *B) {
+int elim_fl_A_ml_block(sm_fl_ml_t *A, sbm_fl_t *B, int nthrds) {
   return 0;
+}
+
+int elim_fl_A_blocks_task(sbm_fl_t *A, sbm_fl_t *B, ci_t block_col_idx_B, ri_t nbrows_A) {
+  bi_t i;
+  ri_t j, k;
+  //re_l_t *dense_block[B->bheight] __attribute__((aligned(0x1000)));
+  re_l_t **dense_block  = (re_l_t **)malloc(B->bheight * sizeof(re_l_t *));
+  ci_t size = B->bwidth * sizeof(re_l_t);
+  for (i=0; i<B->bheight; ++i) {
+    posix_memalign((void **)&dense_block[i], 16, size);
+  }
+
+  for (j=0; j<nbrows_A; ++j) {
+    const ri_t first_block_idx  = 0;
+    // TODO: Martani uses a minimum possibly for zero blocks at the end
+    const ri_t last_block_idx   = nbrows_A- (nbrows_A - 1) +1;
+
+    // set dense block entries to zero
+    for (k=0; k<B->bheight; ++k)
+      memset(dense_block[k], 0, size);
+
+    // copy sparse block data to dense representation
+    if (B->blocks[j][block_col_idx_B] != NULL)
+      copy_sparse_to_dense_block(B->blocks[j][block_col_idx_B], dense_block, B->bheight, B->bwidth);
+
+    for (k=0; k<last_block_idx; ++k)
+      red_with_rectangular_block(A->blocks[j][k], B->blocks[j][k], dense_block, B->bheight);
+
+    red_with_triangular_block(A->blocks[j][last_block_idx], B->blocks[j][k], dense_block, B->bheight);
+
+  }
+  for (i=0; i<B->bheight; ++i)
+    free(dense_block[i]);
+
+  return 0;
+}
+
+void red_with_triangular_block(mbl_t *block_A, mbl_t *block_B, re_l_t **dense_B, ri_t bheight) {
+  bi_t i;
+  if (block_A == NULL || block_B == NULL)
+    return;
+
+  for (i=0; i<bheight/2; ++i) {
+
+  }
+}
+void red_with_rectangular_block(mbl_t *block_A, mbl_t *block_B, re_l_t **dense_B, ri_t bheight) {
+  bi_t i;
+  if (block_A == NULL || block_B == NULL)
+    return;
+
+  for (i=0; i<bheight/2; ++i) {
+
+  }
 }
--- src/elimination.h
+++ src/elimination.h
@@ -11,6 +11,62 @@
 #include <mapping.h>
 
 /**
+ * \brief Copies entry from sparse block sparse_block to dense block dense_block
+ * for elimination purposes.
+ *
+ * \param sparse block sparse_block
+ *
+ * \param dense block dense_block
+ *
+ * \param block height bheight
+ *
+ * \param block width bwidth
+ */
+static inline void copy_sparse_to_dense_block(
+    mbl_t *sparse_block, re_l_t **dense_block, int bheight, int bwidth) {
+  bi_t i,j;
+  for (i=0; i<bheight/2; ++i) {
+    mli_t idx;
+    re_t val1, val2;
+
+    if (sparse_block[i].sz == 0)
+      continue;
+
+    for (j=0; j<sparse_block[i].sz; ++j) {
+      idx   = sparse_block[i].idx[j];
+      dense_block[2*i][idx] = sparse_block[i].val[2*j];
+      dense_block[2*i+1][idx] = sparse_block[i].val[2*j+1];
+    }
+  }
+}
+
+/**
+ * \brief Reduces dense block block_B with rectangular sparse block block_A.
+ *
+ * \param sparse block block_A
+ *
+ * \param sparse block block_B
+ *
+ * \param dense block dense_B
+ *
+ * \param block height bheight
+ */
+void red_with_rectangular_block(mbl_t *block_A, mbl_t *block_B, re_l_t **dense_B, ri_t bheight);
+
+/**
+ * \brief Reduces dense block block_B with triangular sparse block block_A.
+ *
+ * \param sparse block block_A
+ *
+ * \param sparse block block_B
+ *
+ * \param dense block dense_B
+ *
+ * \param block height bheight
+ */
+void red_with_triangular_block(mbl_t *block_A, mbl_t *block_B, re_l_t **dense_B, ri_t bheight);
+
+/**
  * \brief Elimination procedure which reduces the block submatrix A to the unit
  * matrix. Corresponding changes in block submatrix B are carried out, too.
  *
@@ -18,9 +74,26 @@
  *
  * \param block submatrix B (right upper side)
  *
+ * \param number of threads nthrds
+ *
  * \return 0 if success, 1 if failure
  */
-int elim_fl_A_block(sbm_fl_t *A, sbm_fl_t *B);
+int elim_fl_A_block(sbm_fl_t *A, sbm_fl_t *B, int nthrds);
+
+/**
+ * \brief Different block tasks when reducing block submatrix A.
+ *
+ * \param block submatrix A (left upper side)
+ *
+ * \param block submatrix B (right upper side)
+ *
+ * \param column index of blocks in B block_col_idx_B
+ *
+ * \param number of block rows in A nblock_rows_A
+ *
+ * \return 0 if success, 1 if failure
+ */
+int elim_fl_A_blocks_task(sbm_fl_t *A, sbm_fl_t *B, ci_t block_col_idx_B, ri_t nbrows_A);
 
 /**
  * \brief Elimination procedure which reduces the multiline submatrix A
@@ -30,8 +103,10 @@ int elim_fl_A_block(sbm_fl_t *A, sbm_fl_t *B);
  *
  * \param block submatrix B (right upper side)
  *
+ * \param number of threads nthrds
+ *
  * \return 0 if success, 1 if failure
  */
-int elim_fl_A_ml_block(sm_fl_ml_t *A, sbm_fl_t *B);
+int elim_fl_A_ml_block(sm_fl_ml_t *A, sbm_fl_t *B, int nthrds);
 
 #endif
