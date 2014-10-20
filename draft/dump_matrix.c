6913f7c4b5de98e14b7baf6fe3feef5807dccedc
Brice Boyer <brice.boyer@lip6.fr>
2014-10-20 18:39:15 +0200

    ansi + comments

--- draft/dump_matrix.c
+++ draft/dump_matrix.c
@@ -47,11 +47,11 @@ typedef I32 Boolean;
 typedef void* Any;
 
 #include <stdlib.h>
-#include <string.h> // strcmp
+#include <string.h> /* strcmp */
 #include <stdio.h>
 #include <assert.h>
 
-#include <stdint.h> // uint32_t et cie.
+#include <stdint.h> /* uint32_t et cie. */
 
 
 
@@ -77,7 +77,7 @@ static void dump_matrix(char *fic,int all,int strict,int magma)
   FILE* f=ouvrir(fic,"r");
   unsigned short int *nz;
   unsigned int       *pos;
-  //unsigned int       *row;
+  /* unsigned int       *row; */
   unsigned int        *sz;
   unsigned int n;
   unsigned int m;
@@ -157,7 +157,7 @@ static void dump_matrix(char *fic,int all,int strict,int magma)
 	 __typeof__ (b) _b = (b); \
 	 _a > _b ? _a : _b; })
 
-#define NEGMASK  (1<<31)
+#define NEGMASK  (1U<<31)
 
 #define SAFE_MALLOC(ptr,size,elt) \
 	        elt * ptr = (elt *) malloc(size*sizeof(elt)); \
@@ -198,7 +198,6 @@ static void dump_matrix(char *fic,int all,int strict,int magma)
 
 	fprintf(stderr,"proposed format printing");
 
-	//size_t n_imp;
 	SAFE_READ_V(m,  uint32_t,fh);
 	SAFE_READ_V(n,  uint32_t,fh);
 	SAFE_READ_V(mod,uint32_t,fh);
@@ -218,26 +217,26 @@ static void dump_matrix(char *fic,int all,int strict,int magma)
 	{
 		uint32_t i;
 
-		// start_zo
-		SAFE_READ_P(start_zo, m+1, uint32_t, fh);
+		/* start_zo */
+		SAFE_READ_P(start_zo, m+1, uint64_t, fh); /* last element is an uint64_t. I don't beleive it is necessary to compress (as in row_length on uint16_t for instance but saving 3m/4, really ?) */
 
-		// map_pol_zo correspondance
-		SAFE_READ_P(map_pol_zo,m,uint32_t,fh);
+		/* map_pol_zo correspondance */
+		SAFE_READ_P(map_pol_zo,m,uint32_t,fh); /* rows are numbered on 32 bits */
 
-		// if compressed, we need to know the size of colid_zo
-		SAFE_READ_V(colid_zo_size,uint32_t,fh);
+		/* if compressed, we need to know the size of colid_zo */
+		SAFE_READ_V(colid_zo_size,uint32_t,fh); /* the size may be large (but < nnz/2) */
 
-		// colid_zo
-		SAFE_READ_P(colid_zo,colid_zo_size,uint32_t,fh);
+		/* colid_zo */
+		SAFE_READ_P(colid_zo,colid_zo_size,uint32_t,fh); /*   columns are numbered on 32 bits */
 
-		// nnz_pol
-		SAFE_READ_V(nnz_pol,uint64_t,fh);
+		/* nnz_pol */
+		SAFE_READ_V(nnz_pol,uint64_t,fh); /* number of coefficients may be large */
 
-		// start_pol
-		SAFE_READ_P(start_pol,m,uint32_t,fh);
+		/* start_pol */
+		SAFE_READ_P(start_pol,m,uint64_t,fh); /* last number is uint64_t. Can be shrunk to save some space. at most m/4 i guess. */
 
-		// vals_pol
-		SAFE_READ_P(vals_pol,nnz_pol,int32_t,fh);
+		/* vals_pol */
+		SAFE_READ_P(vals_pol,nnz_pol,int32_t,fh); /* elements are int32_t (or anything else) */
 
 
 		if (magma)
@@ -257,18 +256,22 @@ static void dump_matrix(char *fic,int all,int strict,int magma)
 				printf("%u\n%u\n",m,n);
 
 		for(i=0;i<m;i++) {
-			int32_t * vals_pol_begin = vals_pol[start_pol[map_pol_zo[i]]];
-			uint32_t v ;
-			// uint32_t col = 0 ;
-			uint32_t j = start_zo[i] ; // C99 inside for :-(
+			/* pointer to the values of the polynomial corresponding to row i  */
+			int32_t * vals_pol_begin = vals_pol + start_pol[map_pol_zo[i]];
+			uint32_t v ; /* just the value */
+			uint32_t j = start_zo[i] ; /* C99 inside for :-( */
 			for (  ; j < start_zo [i+1] ; ) {
+				/* NEGMASK flag (last bit set) says that column
+				 * is next column for that line has 0 ;
+				 * otherwise, next element is the number of
+				 * consecutive columns with non zeros at this
+				 * row. */
 				uint32_t first = colid_zo[j++] ;
-				uint32_t repet = ((first & NEGMASK)==NEGMASK)?1:(colid_zo[j++]);
-				first ^= NEGMASK ;
-				// repet col after first
-				uint32_t k = 0 ; // C99 inside for :-(
+				uint32_t repet = ((first & NEGMASK) == NEGMASK)?1:(colid_zo[j++]);
+				first ^= NEGMASK ; /* get the actual first column by unmasking */
+				uint32_t k = 0 ; /* C99 for this inside for :-( */
 				for ( ; k < repet ; ++k) {
-					v = vals_pol_begin[k];
+					v = vals_pol_begin[k]; /* consecutive values */
 
 					/* fprintf(stderr,"<%u>",szi); */
 					if (magma)
@@ -277,8 +280,8 @@ static void dump_matrix(char *fic,int all,int strict,int magma)
 						printf("%u %u %d\n",i+1,first+k+1,v);
 
 				}
-				// assert something sur first
-				vals_pol_begin += repet ;
+				/* assert something sur first */
+				vals_pol_begin += repet ; /* jump to the next "first" */
 			}
 		}
 		fprintf(stderr,"\n");
