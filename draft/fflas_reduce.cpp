e779f5dcb3684c987ce59a288cb7f1c88448ec43
Brice Boyer <brice.boyer@lip6.fr>
2015-02-11 15:56:50 +0100

    update to latest givaro

--- draft/fflas_reduce.cpp
+++ draft/fflas_reduce.cpp
@@ -18,10 +18,10 @@ void     Finit_double  (double p, const double * A, double * B, uint64_t n);
 
 uint32_t RowReduce_uint32_t( uint32_t p, uint32_t * A, uint32_t m, uint32_t n, uint32_t lda)
 {
-	size_t * P  = FFLAS::fflas_new<size_t>(max(m,n)) ;
-	size_t * Qt = FFLAS::fflas_new<size_t>(max(m,n)) ;
+	size_t * P  = FFLAS::fflas_new<size_t>(std::max(m,n)) ;
+	size_t * Qt = FFLAS::fflas_new<size_t>(std::max(m,n)) ;
 
-	FFPACK::Modular<uint32_t> F(p);
+	Givaro::Modular<uint32_t> F(p);
 	// uint32_t r = FFPACK::LUdivine(F,FFLAS::FflasUnit,FFLAS::FflasNoTrans,m,n,A,lda,P,Qt);
 	uint32_t r = FFPACK::RowEchelonForm(F,m,n,A,lda,P,Qt);
 
@@ -32,10 +32,10 @@ uint32_t RowReduce_uint32_t( uint32_t p, uint32_t * A, uint32_t m, uint32_t n, u
 
 uint32_t RowReduce_double( double p, double * A, uint32_t m, uint32_t n, uint32_t lda, uint32_t nt)
 {
-	size_t * P  = FFLAS::fflas_new<size_t>(max(m,n)) ;
-	size_t * Qt = FFLAS::fflas_new<size_t>(max(m,n)) ;
+	size_t * P  = FFLAS::fflas_new<size_t>(std::max(m,n)) ;
+	size_t * Qt = FFLAS::fflas_new<size_t>(std::max(m,n)) ;
 
-	FFPACK::Modular<double> F(p);
+	Givaro::Modular<double> F(p);
 	uint32_t r ; // = FFPACK::LUdivine(F,FFLAS::FflasUnit,FFLAS::FflasNoTrans,m,n,A,lda,P,Qt);
 #ifndef _OPENMP
 	r = FFPACK::RowEchelonForm(F,m,n,A,lda,P,Qt);
@@ -53,12 +53,12 @@ uint32_t RowReduce_double( double p, double * A, uint32_t m, uint32_t n, uint32_
 
 void Freduce_double(double p, double * A, uint64_t n)
 {
-	FFPACK::Modular<double> F(p);
+	Givaro::Modular<double> F(p);
 	FFLAS::freduce(F,n,A,1);
 }
 
 void Finit_double(double p, const double * A, double * B, uint64_t n)
 {
-	FFPACK::Modular<double> F(p);
+	Givaro::Modular<double> F(p);
 	FFLAS::finit(F,n,A,1,B,1);
 }
