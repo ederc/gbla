2f8ebcf5bab0012b2e3b0acd2842aedde092d9c0
Christian Eder <ederc@mathematik.uni-kl.de>
2018-12-21 10:22:53 +0100

    license change from GPLv2 to LGPLv2.1

--- cli/io.c
+++ cli/io.c
@@ -1,20 +1,20 @@
 /* gbla: Gröbner Basis Linear Algebra
- * Copyright (C) 2015 Christian Eder <ederc@mathematik.uni-kl.de>
- *                    Brice Boyer <brice.boyer@lip6.fr>
  * This file is part of gbla.
- * gbla is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- * gbla is distributed in the hope that it will be useful,
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * You should have received a copy of the GNU General Public License
- * along with gbla . If not, see <http://www.gnu.org/licenses/>.
- */
-
-
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA  02110-1301  USA, or see  <http://www.gnu.org/licenses/>. */
 
 
 #include "io.h"
